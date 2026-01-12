b11027d8afe7261e6230327ecd364228c0c13353
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2026-01-12 17:07:22 +0900

    Add new route paths for classes, homeworks, and profile

--- src/router/paths.ts
+++ src/router/paths.ts
@@ -1,4 +1,7 @@
 export const Paths = {
   HOME: '/' as const,
   LOGIN: '/login' as const,
+  CLASSES: '/classes' as const,
+  HOMEWORKS: '/homeworks' as const,
+  PROFILE: '/profile' as const,
 } as const;
